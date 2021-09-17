c069ca0dec4a5dacfc0f3c480253a8262170abc0
Thiago Henrique Fragata <thiago.henrique.fragata>
2021-09-17 14:11:40 -0400

    @Page Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
+import React from 'react';
 import { GetStaticProps } from 'next';
+import Link from 'next/link';
 
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import Header from '../components/Header';
+
+import { FiCalendar } from 'react-icons/fi';
+import { FiUser } from 'react-icons/fi';
 
 interface Post {
   uid?: string;
@@ -24,9 +30,30 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <div className={commonStyles.container}>
+      <Header />
+      <main className={styles.posts}>
+        <Link href="/">
+          <a className={styles.post}>
+            <strong>Title</strong>
+            <p>subtitle</p>
+            <span>
+              <FiCalendar />
+              14 de mar 2021
+            </span>
+            <span>
+              <FiUser />
+              Thiago Fragata
+            </span>
+          </a>
+        </Link>
+        <button type="button">Carregar mais posts</button>
+      </main>
+    </div>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
